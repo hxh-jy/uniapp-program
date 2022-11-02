79ea9d562302ff29d3ffca38c0c702fb39390610
hanxuehong <hanxuehong@excean.com>
2022-11-02 10:51:07 +0800

    商品详情页的开发

--- components/goods/goodsDetail.vue
+++ components/goods/goodsDetail.vue
@@ -1,8 +1,79 @@
 <template>
+	<view>
+		<!-- 商品信息区域 -->
+		<view class="goods-info-box">
+			<view class="price" v-if="info.goods_price">￥{{info.goods_price}}</view>
+			<view class="goods-info-body" v-if="info.goods_name">
+				<view class="goods-name">{{info.goods_name}}</view>
+				<view class="favi">
+					<uni-icons type="star" size="18" color="gray"></uni-icons>
+					<text>收藏</text>
+				</view>
+			</view>
+			<view class="yf">快递：免运费</view>
+		</view>
+		<!-- 商品详情信息 -->
+		<rich-text v-if="info.goods_introduce" :nodes="info.goods_introduce"></rich-text>
+	</view>
 </template>
 
 <script>
+	export default {
+		props: {
+			info: {
+				type: Object,
+				default () {
+					return {}
+				}
+			}
+		},
+		methods: {
+			formateIntro(intro) {
+				return intro.replace(/<img/g,'<img style="display: block;"').repalce(/webp/g,'jpg')
+			}
+		}
+	}
 </script>
 
-<style>
+<style lang="scss">
+	// 商品信息区域的样式
+	.goods-info-box {
+		padding: 10px;
+		padding-right: 0;
+
+		.price {
+			color: #c00000;
+			font-size: 18px;
+			margin: 10px 0;
+		}
+
+		.goods-info-body {
+			display: flex;
+			justify-content: space-between;
+
+			.goods-name {
+				font-size: 13px;
+				padding-right: 10px;
+			}
+
+			// 收藏区域
+			.favi {
+				width: 120px;
+				font-size: 12px;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				border-left: 1px solid #efefef;
+				color: gray;
+			}
+		}
+
+		// 运费
+		.yf {
+			margin: 10px 0;
+			font-size: 12px;
+			color: gray;
+		}
+	}
 </style>
--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,28 +1,77 @@
 <template>
-	<view>
+	<view class="goods-detail">
 		<SwiperPrivew :list="detailInfo.pics" v-if="detailInfo.pics &&　detailInfo.pics.length > 0"></SwiperPrivew>
 		<!-- 商品信息详情区域 -->
+		<GoodsDetail :info="detailInfo"></GoodsDetail>
+		<!-- 商品底部导航区域 -->
+		
+		<!-- fill 控制右侧按钮的样式 -->
+		<!-- options 左侧按钮的配置项 -->
+		<!-- buttonGroup 右侧按钮的配置项 -->
+		<!-- click 左侧按钮的点击事件处理函数 -->
+		<!-- buttonClick 右侧按钮的点击事件处理函数 -->
+		<uni-goods-nav 
+		:fill="true"
+		:options="options" 
+		:buttonGroup="buttonGroup"
+		@click="handleClick"
+		@buttonClick="buttonClick"
+		></uni-goods-nav>
 	</view>
 </template>
 
 <script>
 	import SwiperPrivew from '@/components/goods/SwiperPrivew.vue'
+	import GoodsDetail from '@/components/goods/GoodsDetail'
 	export default {
 		data() {
 			return {
-				detailInfo: {}
+				detailInfo: {},
+				//  定义商品导航栏数据
+				options: [{
+					icon: 'shop',
+					text: '店铺'
+				}, {
+					icon: 'cart',
+					text: '购物车'
+				}],
+				buttonGroup: [{
+						text: '加入购物车',
+						backgroundColor: '#ff0000',
+						color: '#fff'
+					},
+					{
+						text: '立即购买',
+						backgroundColor: '#ffa200',
+						color: '#fff'
+					}
+				]
 			};
 		},
 		components: {
-			SwiperPrivew
+			SwiperPrivew,
+			GoodsDetail
 		},
-		methods:  {
+		methods: {
 			async getGoodsDetail(goods_id) {
-				let res = await uni.api.getGoodsDetail({goods_id: goods_id})
+				let res = await uni.api.getGoodsDetail({
+					goods_id: goods_id
+				})
 				if (res.meta.status == 200) {
 					this.detailInfo = res.message
-					console.log('获取商品详情页数据',res.message)
+					console.log('获取商品详情页数据', res.message)
 				} else return uni.$showMsg('商品详情数据正在加载中')
+			},
+			handleClick(e) {
+				if (e.content.text === '购物车') {
+					uni.switchTab({
+						url: '/pages/cart/cart'
+					})
+				}
+				console.log('左侧被点击勒',e)
+			},
+			buttonClick(e) {
+				console.log('购物车被点击勒',e)
 			}
 		},
 		onLoad(options) {
@@ -32,5 +81,11 @@
 </script>
 
 <style lang="scss">
-
+	.goods-detail {
+		uni-goods-nav {
+			width: 100%;
+			position: fixed;
+			bottom: 0;
+		}
+	}
 </style>
